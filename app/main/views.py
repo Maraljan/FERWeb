b6b260b731c2d69c38a8d979e6c7e8fb2d6ef86b
Ashirova Maral <ashirova310197@gmail.com>
2021-06-05 17:59:45 +0300

    added base webcam page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
-from flask import render_template
+import base64
+from io import BytesIO
 
+from flask import render_template, send_file
+import requests
+
+from config import Config
 from . import main
 from .forms import ImageForm, VideoForm
 
@@ -14,13 +19,32 @@ def webcam():
     return render_template('webcam.html')
 
 
-@main.route('/image')
+@main.route('/image', methods=['GET', 'POST'])
 def image():
     form = ImageForm()
-    return render_template('image.html', form=form)
+    context = {'form': form}
+    if form.validate_on_submit():
+        img = form.img.data.read()
+        files = {'image': img}
+        # response = requests.post(Config.FER_IMAGE_URL, files=files)
+        # response.raise_for_status()
+        # context['len_content'] = len(img)
+        img_base64 = base64.b64encode(img).decode('utf-8')
+        context['img_base64'] = img_base64
+    return render_template('image.html', **context)
 
 
-@main.route('/video')
+@main.route('/video', methods=['GET', 'POST'])
 def video():
     form = VideoForm()
-    return render_template('video.html', form=form)
+    context = {'form': form}
+    if form.validate_on_submit():
+        video_ = form.video.data.read()
+        files = {'video': video_}
+        response = requests.post(Config.FER_VIDEO_URL, files=files)
+        response.raise_for_status()
+        buffer = BytesIO()
+        buffer.write(response.content)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name='video.mp4')
+    return render_template('video.html', **context)
